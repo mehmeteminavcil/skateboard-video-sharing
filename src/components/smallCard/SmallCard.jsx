619dd416c1a2016d3ce9b190b0310388b9dca47c
Mehmet Emin Avcıl <mehmeteminavcill@gmail.com>
2023-03-21 17:04:16 +0300

    smallCards and Video component added

--- src/components/smallCard/SmallCard.jsx
+++ src/components/smallCard/SmallCard.jsx
@@ -1,19 +1,36 @@
-import "./smallCard.scss"
+import "./smallcard.scss";
+import Video from "../video/Video";
+import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
 
-
-const Smallcard = () => {
+const SmallCard = ({ videoSrc }) => {
   return (
-    <div className='smallCard'>
-        <div className="top">
-            video
-        </div>
-        <div className="bottom">
-            <h3 className="authorName">Andys William</h3>
-            <h1 className="cardTitle">Basic how to ride your skateboard comforthly</h1>
-        </div>
-       
-    </div>
-  )
-}
+    <div className="smallCard">
+      <div className="cardTime">7 min</div>
+      <div className="top">
+        <Video videoSrc={videoSrc} />
+        <div className="authorAvatar">
+          <div className="authorImg">
+            <img
+              src="https://images.pexels.com/photos/428364/pexels-photo-428364.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+            />
 
-export default Smallcard
+            <CheckRoundedIcon className="icon" />
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="authorName">
+          Andy William
+          <div className="onlineStatus"></div>
+        </div>
+        <h1 className="cardTitle">
+          Basics show to ride your skateboard comforthly
+        </h1>
+        <span className="authorInfo">53K views · 2 weeks ago</span>
+      </div>
+    </div>
+  );
+};
+
+export default SmallCard;
---
+++ src/components/video/Video.jsx
@@ -0,0 +1,22 @@
+import "./video.scss"
+import HoverVideoPlayer from 'react-hover-video-player';
+
+
+const Video = ({videoSrc}) => {
+  return (
+    <HoverVideoPlayer
+    className="videoContainer"
+    videoSrc={videoSrc}
+   
+
+    videoClassName="player-video"
+    loadingOverlay={
+      <div className="loading-overlay">
+        <div className="loading-spinner" />
+      </div>
+    }
+  />
+  )
+}
+
+export default Video
